ea38f82e787acf3fa610452691625d1f62938d92
Sensei <eat@joes.com>
2023-08-22 14:22:57 +0000

    5

--- C#/Trivia/Tests/Game_should.cs
+++ C#/Trivia/Tests/Game_should.cs
@@ -5,19 +5,16 @@ using Xunit;
 
 public class Game_should
 {
-    [Fact]
-    public void add_player_to_game()
-    {
-        const string playerName = "playerName";
-        var subject = new Game();
-        subject.Add(playerName);
-        subject.HowManyPlayers().ShouldBe(1);
-    }
-        const string playerName = "playerName";
-        const string playerName2 = "playerName2";
+    const string playerName = "playerName";
+    const string playerName2 = "playerName2";
+    const string playerName3 = "playerName3";
+    const string playerName4 = "playerName4";
+    const string playerName5 = "playerName5";
     [Theory]
+    [InlineData(1,playerName)]
     [InlineData(2,playerName,playerName2)]
-    public void add_two_players(int howMany,params string[] players)
+    [InlineData(5,playerName,playerName2,playerName3,playerName4,playerName5)]
+    public void add_players(int howMany,params string[] players)
     {
         var subject = new Game();
         foreach(var player in players)
