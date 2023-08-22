d14df058efd0408ba8eb0f98e72d8013e08948da
Sensei <eat@joes.com>
2023-08-22 14:19:00 +0000

    refactor test

--- C#/Trivia/Tests/Game_should.cs
+++ C#/Trivia/Tests/Game_should.cs
@@ -13,4 +13,17 @@ public class Game_should
         subject.Add(playerName);
         subject.HowManyPlayers().ShouldBe(1);
     }
+    [Fact]
+    public void add_two_players()
+    {
+        const string playerName = "playerName";
+        const string playerName2 = "playerName2";
+        var players = new string[]{playerName,playerName2};
+        var subject = new Game();
+        foreach(var player in players)
+        {
+            subject.Add(player);
+        }
+        subject.HowManyPlayers().ShouldBe(2);
+    }
 }
