349f44fe1f04608e6da5fea4c5f2ff1b04b7f5d9
Sensei <eat@joes.com>
2023-08-23 17:17:59 +0000

    can add five players

--- C#/Trivia/Tests/Game_should.cs
+++ C#/Trivia/Tests/Game_should.cs
@@ -13,5 +13,59 @@ public class Game_should
         game.IsPlayable().ShouldBe(false);
     }
 
+    [Fact]
+    public void Game_Should_Have_One_Player()
+    {
+        Game game = new Game();
+        game.Add("George");
+        game.HowManyPlayers().ShouldBe(1);
+        game.IsPlayable().ShouldBe(false);
+    }
+
+    [Fact]
+    public void Game_Should_Allow_Two_Players()
+    {
+        Game game = new Game();
+        game.Add("George");
+        game.Add("Lenny");
+        game.HowManyPlayers().ShouldBe(2);
+        game.IsPlayable().ShouldBe(true);
+    }
+
+    [Fact]
+    public void Game_Should_Allow_Three_Players()
+    {
+        Game game = new Game();
+        game.Add("George");
+        game.Add("Lenny");
+        game.Add("Rabbit");
+        game.HowManyPlayers().ShouldBe(3);
+        game.IsPlayable().ShouldBe(true);
+    }
+
+    [Fact]
+    public void Game_Should_Allow_Four_Players()
+    {
+        Game game = new Game();
+        game.Add("George");
+        game.Add("Lenny");
+        game.Add("Rabbit");
+        game.Add("Curly's Wife");
+        game.HowManyPlayers().ShouldBe(4);
+        game.IsPlayable().ShouldBe(true);
+    }
+
+[Fact]
+    public void Game_Should_Allow_Five_Players()
+    {
+        Game game = new Game();
+        game.Add("George");
+        game.Add("Lenny");
+        game.Add("Rabbit");
+        game.Add("Curly's Wife");
+        game.Add("Old Guy");
+        game.HowManyPlayers().ShouldBe(5);
+        game.IsPlayable().ShouldBe(true);
+    }
 
 }
