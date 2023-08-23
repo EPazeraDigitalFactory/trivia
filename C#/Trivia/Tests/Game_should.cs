91f2d4540a4f29d56fbc9256db03bbc1d3d3c239
Sensei <eat@joes.com>
2023-08-23 16:48:41 +0000

    a failing test

---
+++ C#/Trivia/Tests/Game_should.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+public class Game_should
+{
+    [Fact]
+    public void Do_something()
+    {
+        throw new NotImplementedException("TODO: make a better test");
+    }
+}
