fe0fae79d4700786d6ae05f13a90e45f4eb0af02
Sensei <eat@joes.com>
2023-08-29 14:50:47 +0000

    allow_geting_out_of_penalty_box

--- C#/Trivia/Tests/Game_should.cs
+++ C#/Trivia/Tests/Game_should.cs
@@ -81,4 +81,59 @@ public class Game_should
         output[7].ShouldContain($"The category is {expectedCategory}");
         output[8].ShouldContain($"{expectedCategory} Question 0");
     }
-}
+    [Fact]
+    public void allow_wrong_answers()
+    {
+        const int expectedMessageCount=11;
+        var dieRoll=1;
+        var expectedLocation=dieRoll;
+        var expectedCategory="Science";
+
+        var output = new List<string>();
+        var subject = new Game(output.Add);
+        subject.Add(playerName);
+        subject.Add(playerName2);
+        subject.Roll(dieRoll);
+        subject.WrongAnswer().ShouldBe(true);
+        output.Count.ShouldBe(expectedMessageCount);
+        output[0].ShouldContain($"{playerName} was added");
+        output[1].ShouldContain("They are player number 1");
+        output[2].ShouldContain($"{playerName2} was added");
+        output[3].ShouldContain("They are player number 2");
+        output[4].ShouldContain($"{playerName} is the current player");
+        output[5].ShouldContain($"They have rolled a {dieRoll}");
+        output[6].ShouldContain($"playerName's new location is {expectedLocation}");
+        output[7].ShouldContain($"The category is {expectedCategory}");
+        output[8].ShouldContain($"{expectedCategory} Question 0");
+        output[9].ShouldContain($"Question was incorrectly answered");
+        output[10].ShouldContain($"{playerName} was sent to the penalty box");
+   }
+    [Fact]
+    public void allow_geting_out_of_penalty_box()
+    {
+        const int expectedMessageCount=8;
+        var dieRoll=1;
+        var expectedLocation=dieRoll * 2;
+        var expectedCategory="Sports";
+
+        var output = new List<string>();
+        var subject = new Game(output.Add);
+        subject.Add(playerName);
+        subject.Add(playerName2);
+        subject.Roll(dieRoll);
+        subject.WrongAnswer().ShouldBe(true);
+        subject.Roll(dieRoll);
+        subject.WrongAnswer().ShouldBe(true);
+        output.Clear();
+        subject.Roll(dieRoll);
+        subject.WasCorrectlyAnswered().ShouldBe(true);
+        output.Count.ShouldBe(expectedMessageCount);
+        output[0].ShouldContain($"{playerName} is the current player");
+        output[1].ShouldContain($"They have rolled a {dieRoll}");
+        output[2].ShouldContain($"{playerName} is getting out of the penalty box");
+        output[3].ShouldContain($"playerName's new location is {expectedLocation}");
+        output[4].ShouldContain($"The category is {expectedCategory}");
+        output[5].ShouldContain($"{expectedCategory} Question 0");
+        output[6].ShouldContain($"Answer was correct!!!!");
+        output[7].ShouldContain($"{playerName} now has 1 Gold Coins.");
+   }}
